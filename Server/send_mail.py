6a850c220069e1e75ba985a0913fcfc4d84fcf93
Alexander Braml <alexander.braml@outlook.de>
2022-09-26 23:43:18 +0200

    Extract class in send_mail

--- Server/send_mail.py
+++ Server/send_mail.py
@@ -1,31 +1,22 @@
 import smtplib, ssl
-import time, datetime
 
-from env import server_port, server_host, sender, password, receivers, names
+class Mailer():
+    def __init__(self, sender, password, receivers, server_host="smtp.gmail.com", server_port=465):
+        self.server_host = server_host
+        self.server_port = server_port
+        self.sender = sender
+        self.password = password
+        self.receivers = receivers
+    
+    def send_mail(self, msg):
+        context = ssl.create_default_context()
 
-message_orig = """From: Heizung <heizungsstoerung@braml.eu>
-To: RECEIVERNAME <RECEIVERMAIL>
-MIME-Version: 1.0
-Content-type: text/html
-Subject: STOERUNG
-
-<h3>Die Heizung zeigt eine Stoerung an!</h1>
-<h4>Stoerung war am DATUM um UHRZEIT</h3>
-"""
-
-def send_mails():
-    context = ssl.create_default_context()
-
-    print("Starting to send")
-    with smtplib.SMTP_SSL(server_host, server_port, context=context) as server:
-        get_date = lambda: str(datetime.datetime.fromtimestamp(time.time()).strftime('%d.%m.%Y'))
-        get_time = lambda: str(datetime.datetime.fromtimestamp(time.time()).strftime('%H:%M'))
-        server.login(sender, password)
-        for idx, receiver in enumerate(receivers):
-            message = message_orig
-            message = message.replace('RECEIVERNAME', names[idx])
-            message = message.replace('RECEIVERMAIL', receiver)
-            message = message.replace('DATUM', get_date())
-            message = message.replace('UHRZEIT', get_time())
-            server.sendmail(sender, receiver, message)
-            print("Sent e-mail " + str(idx + 1))
+        print("Starting to send")
+        with smtplib.SMTP_SSL(self.server_host, self.server_port, context=context) as server:
+            
+            server.login(self.sender, self.password)
+            for idx, receiver in enumerate(self.receivers[0]):
+                message = message.replace('RECEIVERNAME', self.receivers[0][idx])
+                message = message.replace('RECEIVERMAIL', receiver)
+                server.sendmail(self.sender, receiver, message)
+                print("Sent e-mail " + str(idx + 1))
