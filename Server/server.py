5c4697cd75196233ab5fc93b366730ed7d17f141
Alexander Braml <alexander.braml@outlook.de>
2022-09-27 00:24:35 +0200

    Implement Pause Logic To Only Send Mail Every Hour

--- Server/server.py
+++ Server/server.py
@@ -4,12 +4,29 @@ from Mailer import Mailer
 from env import sender, password, receivers
 import message as msg
 from ReceiveUDP import listen_forever
+from time import time
+
+
+last_alarm = 0
+next_alarm_earliest = 0
+
+
+def current_seconds():
+    return int(time())
 
 
 def handle_alarm():
-    print("handled alarm")
+    global next_alarm_earliest
+
+    if current_seconds() > next_alarm_earliest:
+        alarm()
+
 
 def alarm():
+    global next_alarm_earliest, last_alarm
+    last_alarm = current_seconds()
+    next_alarm_earliest = last_alarm + 60 * 60
+    print(current_seconds(), "ALARM")
     mailer = Mailer(sender, password, receivers, server_host="smtp.gmail.com", server_port=465)
     mailer.send_mail(msg.get_formatted_msg())
 
