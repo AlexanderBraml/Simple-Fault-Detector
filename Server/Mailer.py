9eb381465cf987e13996236a2a35de95e321a28b
Alexander Braml <alexander.braml@outlook.de>
2022-09-27 09:25:13 +0200

    Add Logging To Mailer

--- Server/Mailer.py
+++ Server/Mailer.py
@@ -1,4 +1,5 @@
 import smtplib, ssl
+from logger import log
 
 class Mailer():
     def __init__(self, sender, password, receivers, server_host="smtp.gmail.com", server_port=465):
@@ -11,11 +12,14 @@ class Mailer():
     def send_mail(self, msg):
         context = ssl.create_default_context()
 
-        print("Starting to send")
+        log("Starting to send e-mails")
         with smtplib.SMTP_SSL(self.server_host, self.server_port, context=context) as server:
             
             server.login(self.sender, self.password)
+            log("Login to smtp server successfull")
             for idx, receiver in enumerate(self.receivers[0]):
                 msg = msg.replace('RECEIVERNAME', self.receivers[0][idx])
                 msg = msg.replace('RECEIVERMAIL', receiver)
                 server.sendmail(self.sender, receiver, msg)
+                log("Sent mail to " + receiver)
+        log("Finished sending e-mails")
